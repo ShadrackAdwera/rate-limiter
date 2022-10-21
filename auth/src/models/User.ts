56df4d88a394e6a73f5a414a861ca7d10de24cc8
ShadrackAdwera <adweshshaddie@gmail.com>
2022-10-21 17:26:57 +0300

    create user schema

--- auth/src/models/User.ts
+++ auth/src/models/User.ts
@@ -1,1 +1,27 @@
-// User
+import { Document, Schema, Model, model } from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
+
+export interface UserDoc extends Document {
+  email: string;
+  password: string;
+  version: number;
+}
+
+export interface UserModel extends Model<UserDoc> {
+  email: string;
+  password: string;
+}
+
+const userSchema = new Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true, toJSON: { getters: true } }
+);
+
+userSchema.set('versionKey', 'version');
+userSchema.plugin(updateIfCurrentPlugin);
+
+const User = model<UserModel, UserDoc>('user', userSchema);
+export { User };
