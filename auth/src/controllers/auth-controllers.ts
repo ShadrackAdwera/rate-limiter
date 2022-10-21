ee1e04d6dab9338567a1d723c80a96ca6a837813
ShadrackAdwera <adweshshaddie@gmail.com>
2022-10-21 17:34:16 +0300

    set up auth controllers

--- auth/src/controllers/auth-controllers.ts
+++ auth/src/controllers/auth-controllers.ts
@@ -1,1 +1,11 @@
-// TDD
+import { Request, Response, NextFunction } from 'express';
+
+const signUp = (req: Request, res: Response, next: NextFunction) => {
+  //
+};
+
+const login = (req: Request, res: Response, next: NextFunction) => {
+  //
+};
+
+export { signUp, login };
