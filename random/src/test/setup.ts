abd7152a769bad96b78f76f21dd0cf300974e9a5
ShadrackAdwera <adweshshaddie@gmail.com>
2022-10-26 14:24:35 +0300

    set up test suite in random service

---
+++ random/src/test/setup.ts
@@ -0,0 +1,40 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+declare global {
+  var getJwt: () => string;
+}
+
+let mongo: MongoMemoryServer;
+
+export const loginRoute = '/api/auth/login';
+
+jest.setTimeout(600000);
+beforeAll(async () => {
+  process.env.JWT_KEY = '=6:$}/N7Hp21HrX[bCiI`xj49xi,_';
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri);
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+  collections.forEach((collection) => collection.deleteMany({}));
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.getJwt = () => {
+  //await await request(app).post(signUpRoute).send(user).expect(201);
+  const token = jwt.sign(
+    { userId: mongoose.Types.ObjectId, email: 'test@mail.com' },
+    process.env.JWT_KEY!,
+    { expiresIn: '1h' }
+  );
+  return token;
+};
