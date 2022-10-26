b0adcaaebc443ac2f68c4047a2a71c9919c7ef98
ShadrackAdwera <adweshshaddie@gmail.com>
2022-10-26 14:44:08 +0300

    configure connection to the DB

---
+++ random/src/index.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+import { app } from './app';
+
+if (!process.env.JWT_KEY) {
+  throw new Error('JWT is not defined!');
+}
+
+if (!process.env.MONGO_URI) {
+  throw new Error('MONGO URI is not defined!');
+}
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI!);
+    app.listen(5001);
+    console.log('Connected to Random Service, listening on PORT: 5001');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
