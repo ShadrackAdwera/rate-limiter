475e620436f94304bb89906177d6dac4b49bf64b
ShadrackAdwera <adweshshaddie@gmail.com>
2022-10-29 11:23:27 +0300

    clean up random test suites

--- random/src/controllers/__test__/random-controllers.test.ts
+++ random/src/controllers/__test__/random-controllers.test.ts
@@ -45,8 +45,12 @@ describe('random controllers', () => {
   });
   describe('get random controller', () => {
     beforeEach(async () => {
-      const newRandom = new Random({ title: 'Random 1', createdBy: userId });
-      await newRandom.save();
+      await request(app)
+        .post(`${baseRandomUrl}/new`)
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${global.getJwt()}`)
+        .send({ title: 'Random Item' })
+        .expect(201);
     });
     it.todo('validates if user is available in the DB returns a 404 if not');
     it('returns a 401 when called without authetication', async () => {
@@ -63,8 +67,12 @@ describe('random controllers', () => {
   });
   describe('get by id random controller', () => {
     beforeEach(async () => {
-      const newRandom = new Random({ title: 'Random 1', createdBy: userId });
-      await newRandom.save();
+      await request(app)
+        .post(`${baseRandomUrl}/new`)
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${global.getJwt()}`)
+        .send({ title: 'Random Item' })
+        .expect(201);
     });
     it.todo('validates if user is available in the DB returns a 404 if not');
     it('returns a 404 if random with the id provided is not found', async () => {
@@ -79,14 +87,18 @@ describe('random controllers', () => {
       return request(app).get(baseRandomUrl).send().expect(401);
     });
     it('fetches random item by id for an authenticated user', async () => {
-      const newRandom = new Random({ title: 'Random 1', createdBy: userId });
-      await newRandom.save();
+      const created = await request(app)
+        .post(`${baseRandomUrl}/new`)
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${global.getJwt()}`)
+        .send({ title: 'Random Item' })
+        .expect(201);
       const response = await request(app)
-        .get(`${baseRandomUrl}/${newRandom.id}`)
+        .get(`${baseRandomUrl}/${created.body.random.id}`)
         .set('Authorization', `Bearer ${global.getJwt()}`)
         .send()
         .expect(200);
-      expect(response.body.random.title).toEqual(newRandom.title);
+      expect(response.body.random.title).toEqual('Random Item');
     });
   });
   describe('update random controller', () => {
