324c3123bf5c9bb728766ec3a7c06e4d0de863cf
ShadrackAdwera <adweshshaddie@gmail.com>
2022-10-29 10:32:56 +0300

    connect to nast on auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,3 +1,4 @@
+import { natsWraper } from '@adwesh/common';
 import mongoose from 'mongoose';
 
 import { app } from './app';
@@ -10,9 +11,33 @@ if (!process.env.MONGO_URI) {
   throw new Error('MONGO URI is not defined!');
 }
 
+if (!process.env.NATS_CLUSTER_ID) {
+  throw new Error('NATS_CLUSTER_ID must be defined');
+}
+
+if (!process.env.NATS_CLIENT_ID) {
+  throw new Error('NATS_CLIENT_ID must be defined');
+}
+
+if (!process.env.NATS_URI) {
+  throw new Error('NATS_URI must be defined');
+}
+
 const start = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI!);
+    await natsWraper.connect(
+      process.env.NATS_CLUSTER_ID!,
+      process.env.NATS_CLIENT_ID!,
+      process.env.NATS_URI!
+    );
+    natsWraper.client.on('close', () => {
+      console.log('NATS shutting down . . .');
+      process.exit();
+    });
+
+    process.on('SIGINT', () => natsWraper.client.close());
+    process.on('SIGTERM', () => natsWraper.client.close());
     app.listen(5000);
     console.log('Connected to Auth Service, listening on PORT: 5000');
   } catch (error) {
