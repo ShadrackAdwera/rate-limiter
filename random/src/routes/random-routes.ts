2bbae047ee52c1c46037d3449f8399ba7cceb80f
ShadrackAdwera <adweshshaddie@gmail.com>
2022-11-01 16:35:57 +0300

    create random items rate limiter middleware

--- random/src/routes/random-routes.ts
+++ random/src/routes/random-routes.ts
@@ -9,12 +9,13 @@ import {
   updateRandom,
   deleteRandom,
 } from '../controllers/random-controllers';
+import { countAndLimitRequests } from '../middlewares/countRequests';
 
 const router = Router();
 
 router.use(checkAuth);
-router.get('', getRandom);
-router.get('/:id', getRandomById);
+router.get('', countAndLimitRequests, getRandom);
+router.get('/:id', countAndLimitRequests, getRandomById);
 router.post('/new', [body('title').trim().isLength({ min: 5 })], addRandom);
 router.patch('/:id', [body('title').trim().isLength({ min: 5 })], updateRandom);
 router.delete('/:id', deleteRandom);
